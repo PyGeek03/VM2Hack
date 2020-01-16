581727cd65c55829a42b59c7279fa9cda67d87f1
PyGeek03 <PyGeek03@users.noreply.github.com>
2020-01-17 00:27:02 +1100

    refactor comparison commands

--- calculation.py
+++ calculation.py
@@ -34,44 +34,56 @@ def write_or() -> tuple:
     )
 
 
-def write_eq(eq_count: int) -> tuple:
-    # initialize EQ subroutine, when eq is called for the first time
-    init = (
-        '@DEF_EQ',
-        '0;JMP  // EQ definition -> jump over',
+def _init_common(sub: str) -> tuple:
+    return (
+       f'@DEF_{sub}',
+       f'0;JMP  // {sub} definition -> jump over',
         '',
-        '(EQ)',
+       f'({sub})',
         '@R15',
         'M=D    // R15 = return address',
 
         *binary_common,
-        'MD=M-D // D = *(SP-2) = *(SP-2) - *(SP-1)',
-        '@:EQ_FLIP:',
-        'D;JEQ  // if *(SP-2) == 0, flip all bits',
-        '       // else: set *(SP-2) = -1 first',
+        'D=M-D  // D = *(SP-2) - *(SP-1)',
+       f'@:TRUE_{sub}:',
+       f'D;J{sub}  // jump if true',
+        '       // else:',
         '@SP    // now M = SP-1',
         'A=M-1  // A = SP-2',
-        'M=-1   // *(SP-2) = -1',
-        '(:EQ_FLIP:)',
-        '@SP    // now M = SP-1',
-        'A=M-1  // A = SP-2',
-        'M=!M   // flip all bits to correct value',
+        'M=0    // false',
         '@R15',
         'A=M',
         '0;JMP  // return',
         '',
-        '(DEF_EQ)',
+       f'(:TRUE_{sub}:)',
+        '@SP    // now M = SP-1',
+        'A=M-1  // A = SP-2',
+        'M=-1   // true',
+        '@R15',
+        'A=M',
+        '0;JMP  // return',
+        '',
+       f'(DEF_{sub})',
         '',
     )
 
-    # call previously initialized EQ
-    call = (
-       f'@RETURN_EQ_{eq_count}',
+
+def _call_common(sub: str, count: int) -> tuple:
+    return (
+       f'@RETURN_{sub}_{count}',
         'D=A',
-        '@EQ',
+       f'@{sub}',
         '0;JMP',
-       f'(RETURN_EQ_{eq_count})',
+       f'(RETURN_{sub}_{count})',
     )
+
+
+def write_eq(eq_count: int) -> tuple:
+    # initialize EQ subroutine, when eq is called for the first time
+    init = _init_common('EQ')
+
+    # call previously initialized EQ
+    call = _call_common('EQ', eq_count)
 
     if eq_count:
         return call
@@ -81,46 +93,10 @@ def write_eq(eq_count: int) -> tuple:
 
 def write_gt(gt_count: int) -> tuple:
     # initialize GT subroutine, when gt is called for the first time
-    init = (
-        '@DEF_GT',
-        '0;JMP  // GT definition -> jump over',
-        '',
-        '(GT)',
-        '@R15',
-        'M=D    // R15 = return address',
-
-        *binary_common,
-        'D=M-D  // D = *(SP-2) - *(SP-1)',
-        '@:TRUE_GT:',
-        'D;JGT  // jump if D > 0',
-        '       // else:',
-        '@SP    // now M = SP-1',
-        'A=M-1  // A = SP-2',
-        'M=0    // false',
-        '@R15',
-        'A=M',
-        '0;JMP  // return',
-        '',
-        '(:TRUE_GT:)',
-        '@SP    // now M = SP-1',
-        'A=M-1  // A = SP-2',
-        'M=-1   // true',
-        '@R15',
-        'A=M',
-        '0;JMP  // return',
-        '',
-        '(DEF_GT)',
-        '',
-    )
+    init = _init_common('GT')
 
     # call previously initialized GT
-    call = (
-       f'@RETURN_GT_{gt_count}',
-        'D=A',
-        '@GT',
-        '0;JMP',
-       f'(RETURN_GT_{gt_count})',
-    )
+    call = _call_common('GT', gt_count)
 
     if gt_count:
         return call
@@ -130,46 +106,10 @@ def write_gt(gt_count: int) -> tuple:
 
 def write_lt(lt_count: int) -> tuple:
     # initialize LT subroutine, when lt is called for the first time
-    init = (
-        '@DEF_LT',
-        '0;JMP  // LT definition -> jump over',
-        '',
-        '(LT)',
-        '@R15',
-        'M=D    // R15 = return address',
-
-        *binary_common,
-        'D=M-D  // D = *(SP-2) - *(SP-1)',
-        '@:TRUE_LT:',
-        'D;JLT  // jump if D < 0',
-        '       // else:',
-        '@SP    // now M = SP-1',
-        'A=M-1  // A = SP-2',
-        'M=0    // false',
-        '@R15',
-        'A=M',
-        '0;JMP  // return',
-        '',
-        '(:TRUE_LT:)',
-        '@SP    // now M = SP-1',
-        'A=M-1  // A = SP-2',
-        'M=-1   // true',
-        '@R15',
-        'A=M',
-        '0;JMP  // return',
-        '',
-        '(DEF_LT)',
-        '',
-    )
+    init = _init_common('LT')
 
     # call previously initialized LT
-    call = (
-       f'@RETURN_LT_{lt_count}',
-        'D=A',
-        '@LT',
-        '0;JMP',
-       f'(RETURN_LT_{lt_count})',
-    )
+    call = _call_common('LT', lt_count)
 
     if lt_count:
         return call
