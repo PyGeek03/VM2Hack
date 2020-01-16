468841297c811f4380ce0c89eb69c5a5d2c4c7a4
PyGeek03 <PyGeek03@users.noreply.github.com>
2020-01-16 17:28:51 +1100

    complete push & pop

--- mem_access.py
+++ mem_access.py
@@ -1,40 +1,58 @@
-def write_push(segment, index):
-    # constant and pointer are "virtual" segments
-    if segment == 'constant': 
-        head = (
-            "@"              + str(index) + "\n"
-            "D=A    // D = " + str(index) + "\n"
-        )
-    elif segment == 'pointer':
-        head = (
-            "@" + str(3+index) +    "\n" # index = 0: this, index = 1: that
-            "D=A    // D = pointer" "\n"
-        )
-    else:
-        proper_segments = {
-            'local'     : "@" + str(1+index),
-            'argument'  : "@" + str(2+index),
-            'this'      : "@" + str(3+index),
-            'that'      : "@" + str(4+index),
-            'temp'      : "@" + str(5+index),
-            'static'    : "@" + index,
-        }
-        head = (
-            proper_segments[segment]  + "\n"
-            "D=M  // D = memory value"  "\n"
-        )
+def find_addr(segment: str, index: str) -> str:
+    try:
+        temp_addr = 5 + int(index)
+    except ValueError:  # only static segment has non-numeric "index" (actually label)
+        return '@%s' % index
 
-    body = (
-        "@0     // A = SP"  "\n"
-        "A=M    // A = *SP" "\n"
-        "M=D    // *SP = D" "\n"
-        "@0     // A = SP"  "\n"
-        "M=M+1  // (*SP)++" "\n"
-        "\n"
+    latt_common = (
+        'D=M    // D = segmentPointer'          '\n'
+        '@%s\n' % index
+        'A=A+D  // addr = segmentPointer + i'   '\n'
     )
+    numeric_segments = {
+        'constant': '@%s' % index,
+        'temp'    : '@%s    // addr = 5 + i' % temp_addr,
+        'pointer' : '@%s' % ('THIS' if index == '0' else 'THAT'),
 
-    return head + body
+        'local'   : '@LCL\n%s'  % latt_common,
+        'argument': '@ARG\n%s'  % latt_common,
+        'this'    : '@THIS\n%s' % latt_common,
+        'that'    : '@THAT\n%s' % latt_common,
+    }
+    return numeric_segments[segment]
 
 
-def write_pop(segment, index):
-    pass
+def write_push(segment: str, index: str) -> list:
+    addr = find_addr(segment, index)
+    return [
+        addr,
+        'D=M    // D = *addr' if segment != 'constant' else 'D=A    // D = %s' % index,
+        '@SP',
+        'A=M',
+        'M=D    // *SP = D',
+        '@SP',
+        'M=M+1  // SP++',
+    ]
+
+
+def write_pop(segment: str, index: str) -> list:
+    addr = find_addr(segment, index)
+    if segment == 'pointer':
+        return [
+            '@SP',
+            'AM=M-1 // SP--',
+            'D=M    // D = *SP',
+            addr,
+            'M=D    // %s = *SP' % ('THIS' if index == '0' else 'THAT'),
+        ]
+    return [
+        addr,
+        'D=A    // D = addr',
+        '@SP',
+        'AM=M-1 // SP--',
+        '// now M = *SP, D = addr, we have to make A = addr, D = *SP',
+        'D=D-M  // addr - *SP',
+        'A=M+D  // *SP + (addr - *SP) = addr!',
+        'D=A-D  // addr - (addr - *SP) = *SP!',
+        'M=D    // *addr = *SP!!!',
+    ]
