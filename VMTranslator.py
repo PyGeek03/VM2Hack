1e00a6ee3dc9529706af17a083add05772d23c3e
PyGeek03 <PyGeek03@users.noreply.github.com>
2020-01-16 17:29:20 +1100

    Fix directory translation mechanism

--- VMTranslator.py
+++ VMTranslator.py
@@ -3,14 +3,27 @@ from compiler import compile
 
 
 def main():
+    '''Main function of the program'''
     input = sys.argv[1]
+
     if input[-3:] == ".vm":
-        compile(input)
-    else:
+        asm_filepath = input[:-3] + ".asm"
+        compile(asm_filepath, [input])
+    elif os.path.isdir(input):
+        # translated .asm file will be inside the directory
+        asm_filepath = os.path.join(input, os.path.basename(input) + ".asm")
         for root, _, files in os.walk(input):
-            for vm_filename in fnmatch.filter(files, '*.vm'):
-                vm_filepath = os.path.join(root, vm_filename)
-                compile(vm_filepath)
+            vm_files = [os.path.join(root, vm_file) for vm_file in fnmatch.filter(files, '*.vm')]
+        if vm_files:
+            compile(asm_filepath, vm_files)
+        else:
+            raise ValueError("Directory does not contain .vm file. Please try again")
+    else:
+        raise ValueError("Not a .vm file nor a directory. Please try again")
+
+    # print the translated .asm file
+    with open(asm_filepath) as asm_file:
+        print(asm_file.read(), end="")
 
 
 if __name__ == "__main__":
