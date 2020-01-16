bd0d007c639dee7836bbe983329d05dadf4d9f90
PyGeek03 <PyGeek03@users.noreply.github.com>
2020-01-16 23:23:16 +1100

    implemented branching commands

--- branching.py
+++ branching.py
@@ -1,10 +1,21 @@
-def write_label(label):
-    pass
+def write_label(label: str) -> tuple:
+    return (
+        f'({label})',
+    )
 
 
-def write_goto(label):
-    pass
+def write_goto(label: str) -> tuple:
+    return (
+        f'@{label}',
+        '0;JMP',
+    )
 
 
-def write_if(label):
-    pass
+def write_if(label: str) -> tuple:
+    return (
+        '@SP',
+        'AM=M-1 // A = SP-1, also SP--',
+        'D=M    // D = *(SP-1)',
+        f'@{label}',
+        'D;JNE  // jump if *(SP-1) != 0',
+    )
