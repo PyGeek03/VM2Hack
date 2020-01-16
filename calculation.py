0b7bb210cdd77febe96d892bf7ab63c036642af2
PyGeek03 <PyGeek03@users.noreply.github.com>
2020-01-17 00:03:24 +1100

    Performance refactoring

--- calculation.py
+++ calculation.py
@@ -1,77 +1,77 @@
-binary_common = [
+binary_common = (
     '@SP',
-    'AM=M-1 // A = *SP-1, also (*SP)--',
-    'D=M    // D = *(*SP-1)',
-    'A=A-1  // A = *SP-2',
-]  # D holds *(*SP-1), M holds *(*SP-2), A holds *SP-2
+    'AM=M-1 // A = SP-1, also SP--',
+    'D=M    // D = *(SP-1)',
+    'A=A-1  // A = SP-2',
+)  # D holds *(SP-1), M holds *(SP-2), A holds SP-2
 
 
-def write_add() -> list:
-    return [
+def write_add() -> tuple:
+    return (
         *binary_common,
-        'M=D+M  // *(*SP-2) += *(*SP-1)',
-    ]
+        'M=D+M  // *(SP-2) += *(SP-1)',
+    )
 
 
-def write_sub() -> list:
-    return [
+def write_sub() -> tuple:
+    return (
         *binary_common,
-        'M=M-D  // *(*SP-2) -= *(*SP-1)',
-    ]
+        'M=M-D  // *(SP-2) -= *(SP-1)',
+    )
 
 
-def write_and() -> list:
-    return [
+def write_and() -> tuple:
+    return (
         *binary_common,
-        'M=D&M  // *(*SP-2) &= *(*SP-1)',
-    ]
+        'M=D&M  // *(SP-2) &= *(SP-1)',
+    )
 
 
-def write_or() -> list:
-    return [
+def write_or() -> tuple:
+    return (
         *binary_common,
-        'M=D|M  // *(*SP-2) |= *(*SP-1)',
-    ]
+        'M=D|M  // *(SP-2) |= *(SP-1)',
+    )
 
 
-def write_eq(eq_count: int) -> list:
-    # initialize :EQ: subroutine, when eq is called for the first time
-    init = [
-        '@DEF_:EQ:',
-        '0;JMP  // :EQ: definition -> jump over',
+def write_eq(eq_count: int) -> tuple:
+    # initialize EQ subroutine, when eq is called for the first time
+    init = (
+        '@DEF_EQ',
+        '0;JMP  // EQ definition -> jump over',
         '',
-        '(:EQ:)',
+        '(EQ)',
         '@R15',
         'M=D    // R15 = return address',
 
         *binary_common,
-        'MD=M-D // D = *(*SP-2) = *(*SP-2) - *(*SP-1)',
+        'MD=M-D // D = *(SP-2) = *(SP-2) - *(SP-1)',
         '@:EQ_FLIP:',
-        'D;JEQ  // if *(*SP-2) == 0, flip all bits',
-        '       // else: set *(*SP-2) = -1 first',
-        '@SP    // now M = *SP-1',
-        'A=M-1  // A = *SP-2',
-        'M=-1   // *(*SP-2) = -1',
+        'D;JEQ  // if *(SP-2) == 0, flip all bits',
+        '       // else: set *(SP-2) = -1 first',
+        '@SP    // now M = SP-1',
+        'A=M-1  // A = SP-2',
+        'M=-1   // *(SP-2) = -1',
         '(:EQ_FLIP:)',
-        '@SP    // now M = *SP-1',
-        'A=M-1  // A = *SP-2',
+        '@SP    // now M = SP-1',
+        'A=M-1  // A = SP-2',
         'M=!M   // flip all bits to correct value',
         '@R15',
         'A=M',
         '0;JMP  // return',
         '',
-        '(DEF_:EQ:)',
+        '(DEF_EQ)',
         '',
-    ]
+    )
 
-    # call previously initialized :EQ:
-    call = [
-        '@RETURN_EQ_%s' % eq_count,
+    # call previously initialized EQ
+    call = (
+       f'@RETURN_EQ_{eq_count}',
         'D=A',
-        '@:EQ:',
+        '@EQ',
         '0;JMP',
-        '(RETURN_EQ_%s)' % eq_count,
-    ]
+       f'(RETURN_EQ_{eq_count})',
+    )
 
     if eq_count:
         return call
@@ -79,48 +79,48 @@ def write_eq(eq_count: int) -> list:
         return init + call
 
 
-def write_gt(gt_count: int) -> list:
-    # initialize :GT: subroutine, when gt is called for the first time
-    init = [
-        '@DEF_:GT:',
-        '0;JMP  // :GT: definition -> jump over',
+def write_gt(gt_count: int) -> tuple:
+    # initialize GT subroutine, when gt is called for the first time
+    init = (
+        '@DEF_GT',
+        '0;JMP  // GT definition -> jump over',
         '',
-        '(:GT:)',
+        '(GT)',
         '@R15',
         'M=D    // R15 = return address',
 
         *binary_common,
-        'D=M-D  // D = *(*SP-2) - *(*SP-1)',
+        'D=M-D  // D = *(SP-2) - *(SP-1)',
         '@:TRUE_GT:',
         'D;JGT  // jump if D > 0',
         '       // else:',
-        '@SP    // now M = *SP-1',
-        'A=M-1  // A = *SP-2',
+        '@SP    // now M = SP-1',
+        'A=M-1  // A = SP-2',
         'M=0    // false',
         '@R15',
         'A=M',
         '0;JMP  // return',
         '',
         '(:TRUE_GT:)',
-        '@SP    // now M = *SP-1',
-        'A=M-1  // A = *SP-2',
+        '@SP    // now M = SP-1',
+        'A=M-1  // A = SP-2',
         'M=-1   // true',
         '@R15',
         'A=M',
         '0;JMP  // return',
         '',
-        '(DEF_:GT:)',
+        '(DEF_GT)',
         '',
-    ]
+    )
 
-    # call previously initialized :GT:
-    call = [
-        '@RETURN_GT_%s' % gt_count,
+    # call previously initialized GT
+    call = (
+       f'@RETURN_GT_{gt_count}',
         'D=A',
-        '@:GT:',
+        '@GT',
         '0;JMP',
-        '(RETURN_GT_%s)' % gt_count,
-    ]
+       f'(RETURN_GT_{gt_count})',
+    )
 
     if gt_count:
         return call
@@ -128,48 +128,48 @@ def write_gt(gt_count: int) -> list:
         return init + call
 
 
-def write_lt(lt_count: int) -> list:
-    # initialize :LT: subroutine, when lt is called for the first time
-    init = [
-        '@DEF_:LT:',
-        '0;JMP  // :LT: definition -> jump over',
+def write_lt(lt_count: int) -> tuple:
+    # initialize LT subroutine, when lt is called for the first time
+    init = (
+        '@DEF_LT',
+        '0;JMP  // LT definition -> jump over',
         '',
-        '(:LT:)',
+        '(LT)',
         '@R15',
         'M=D    // R15 = return address',
 
         *binary_common,
-        'D=M-D  // D = *(*SP-2) - *(*SP-1)',
+        'D=M-D  // D = *(SP-2) - *(SP-1)',
         '@:TRUE_LT:',
         'D;JLT  // jump if D < 0',
         '       // else:',
-        '@SP    // now M = *SP-1',
-        'A=M-1  // A = *SP-2',
+        '@SP    // now M = SP-1',
+        'A=M-1  // A = SP-2',
         'M=0    // false',
         '@R15',
         'A=M',
         '0;JMP  // return',
         '',
         '(:TRUE_LT:)',
-        '@SP    // now M = *SP-1',
-        'A=M-1  // A = *SP-2',
+        '@SP    // now M = SP-1',
+        'A=M-1  // A = SP-2',
         'M=-1   // true',
         '@R15',
         'A=M',
         '0;JMP  // return',
         '',
-        '(DEF_:LT:)',
+        '(DEF_LT)',
         '',
-    ]
+    )
 
-    # call previously initialized :LT:
-    call = [
-        '@RETURN_LT_%s' % lt_count,
+    # call previously initialized LT
+    call = (
+       f'@RETURN_LT_{lt_count}',
         'D=A',
-        '@:LT:',
+        '@LT',
         '0;JMP',
-        '(RETURN_LT_%s)' % lt_count,
-    ]
+       f'(RETURN_LT_{lt_count})',
+    )
 
     if lt_count:
         return call
@@ -177,17 +177,17 @@ def write_lt(lt_count: int) -> list:
         return init + call
 
 
-def write_not() -> list:
-    return [
+def write_not() -> tuple:
+    return (
         '@SP',
-        'A=M-1  // A = *SP-1',
-        'M=!M   // *SP-1 = !(*SP-1)',
-    ]
+        'A=M-1  // A = SP-1',
+        'M=!M   // *(SP-1) = !*(SP-1)',
+    )
 
 
-def write_neg() -> list:
-    return [
+def write_neg() -> tuple:
+    return (
         '@SP',
-        'A=M-1  // A = *SP-1',
-        'M=-M   // *SP-1 = -(*SP-1)',
-    ]
+        'A=M-1  // A = SP-1',
+        'M=-M   // *(SP-1) = -*(SP-1)',
+    )
--- mem_access.py
+++ mem_access.py
@@ -1,58 +1,67 @@
 def find_addr(segment: str, index: str) -> str:
     try:
         temp_addr = 5 + int(index)
-    except ValueError:  # only static segment has non-numeric "index" (actually label)
-        return '@%s' % index
+    except ValueError:
+        # only static segment has non-numeric "index" (actually label)
+        return f'@{index}'
 
     latt_common = (
-        'D=M    // D = segmentPointer'          '\n'
-        '@%s\n' % index
-        'A=A+D  // addr = segmentPointer + i'   '\n'
+        'D=M    // D = segmentPointer'  '\n'
+        f'@{index}'                     '\n'
+        f'A=A+D  // addr = segmentPointer + {index}'
     )
     numeric_segments = {
-        'constant': '@%s' % index,
-        'temp'    : '@%s    // addr = 5 + i' % temp_addr,
-        'pointer' : '@%s' % ('THIS' if index == '0' else 'THAT'),
+        'constant': f'@{index}',
+        'temp'    : f'@{temp_addr}    // addr = 5 + {index}',
+        'pointer' : f"@{'THIS' if index == '0' else 'THAT'}",
 
-        'local'   : '@LCL\n%s'  % latt_common,
-        'argument': '@ARG\n%s'  % latt_common,
-        'this'    : '@THIS\n%s' % latt_common,
-        'that'    : '@THAT\n%s' % latt_common,
+        'local'   : f'@LCL\n{latt_common}',
+        'argument': f'@ARG\n{latt_common}',
+        'this'    : f'@THIS\n{latt_common}',
+        'that'    : f'@THAT\n{latt_common}',
     }
     return numeric_segments[segment]
 
 
-def write_push(segment: str, index: str) -> list:
+def write_push(segment: str, index: str) -> tuple:
     addr = find_addr(segment, index)
-    return [
+    
+    if segment == 'constant':
+        D = f'D=A    // D = {index}'
+    else:
+        D =  'D=M    // D = *addr'
+
+    return (
         addr,
-        'D=M    // D = *addr' if segment != 'constant' else 'D=A    // D = %s' % index,
+        D,
         '@SP',
         'A=M',
         'M=D    // *SP = D',
         '@SP',
         'M=M+1  // SP++',
-    ]
+    )
 
 
-def write_pop(segment: str, index: str) -> list:
+def write_pop(segment: str, index: str) -> tuple:
     addr = find_addr(segment, index)
+
     if segment == 'pointer':
-        return [
+        return (
             '@SP',
             'AM=M-1 // SP--',
             'D=M    // D = *SP',
             addr,
-            'M=D    // %s = *SP' % ('THIS' if index == '0' else 'THAT'),
-        ]
-    return [
-        addr,
-        'D=A    // D = addr',
-        '@SP',
-        'AM=M-1 // SP--',
-        '// now M = *SP, D = addr, we have to make A = addr, D = *SP',
-        'D=D-M  // addr - *SP',
-        'A=M+D  // *SP + (addr - *SP) = addr!',
-        'D=A-D  // addr - (addr - *SP) = *SP!',
-        'M=D    // *addr = *SP!!!',
-    ]
+           f"M=D    // {'THIS' if index == '0' else 'THAT'} = *SP",
+        )
+    else:
+        return (
+            addr,
+            'D=A    // D = addr',
+            '@SP',
+            'AM=M-1 // SP--',
+            '// now M = *SP, D = addr, we have to make A = addr, D = *SP',
+            'D=D-M  // addr - *SP',
+            'A=M+D  // *SP + (addr - *SP) = addr!',
+            'D=A-D  // addr - (addr - *SP) = *SP!',
+            'M=D    // *addr = *SP!!!',
+        )
