1224ce47b9f378c4f5767b997c098bced8b28720
PyGeek03 <PyGeek03@users.noreply.github.com>
2020-01-18 00:36:39 +1100

    major architecture refactoring

--- VMTranslator.py
+++ VMTranslator.py
@@ -1,88 +1,70 @@
 import sys
-import os
-import fnmatch
-from compiler import bootstrap, compile_line
+from pathlib import Path
+from typing import List
 
+from compiler import bootstrap_dir, compile_line
 
-def translate(asm_filepath: str, vm_files: list) -> None:
+def translate(asm_filepath: Path, vm_files: List[Path]) -> None:
     '''Main translator'''
-    # Bootstrap asm file (will overwrite if it already exists)
-    with open(asm_filepath, 'w') as asm_file:
-        for line in bootstrap:
-            asm_file.write(f"{line}\n")
-    
-    # Keep count of "stateful" commands
-    count = {
-        'eq': 0,
-        'lt': 0,
-        'gt': 0,
-        'call': 0,
+    # Initialize the translation state
+    state = {
+        "class"     : "",
+        "function"  : "",
+        "eq_count"  : 0,
+        "lt_count"  : 0,
+        "gt_count"  : 0,
+        "call_count": 0,
     }
+
+    # Bootstrap asm file, overwrite if exists
+    with asm_filepath.open('w') as asm_file:
+        if len(vm_files) > 1:
+            # translating a directory with more than 1 vm file
+            asm_file.writelines(f"{line}\n" for line in bootstrap_dir)
+        else:
+            # simply clean old asm file, if exists
+            asm_file.write('')
 
     # Translate all vm file in the list into the asm file
     for vm_filepath in vm_files:
-        # accessing static segment & branching is scoped, but differently
-        vm_filename = os.path.basename(vm_filepath)[:-3]
-        scope = {
-            'filename': vm_filename,           # static segment's scope
-            'function': [f"{vm_filename}.0"],  # branching's scope
-        }
+        # each file is a new class
+        class_name = vm_filepath.stem
+        state = {**state, "class": class_name, "function": f"{class_name}.0"}
 
-        with open(vm_filepath) as vm_file, open(asm_filepath, 'a') as asm_file:
+        with vm_filepath.open() as vm_file, asm_filepath.open('a') as asm_file:
             # Compile line by line
             for vm_line in vm_file:
                 # Document the vm source line
                 asm_doc = f"// {vm_line}"
 
-                # Tokenize and parse the vm source line
-                tokens = vm_line.split("//")[0].split()  # ignore comments
-                try:
-                    command, *args = tokens
-                except ValueError:  # blank line
-                    asm_block = ()
-                else:
-                    # Compile!
-                    asm_block = compile_line(command, args, scope, count)
-                    # (if needed) update scope & count
-                    if command == 'function':
-                        function_name = args[1]
-                        scope['function'].append(function_name)
-                    elif command == 'return':
-                        scope['function'].pop()
-                    elif command in count:
-                        count[command] += 1
+                # Compile & update state
+                asm_block, state = compile_line(vm_line, state)
 
                 # Finally, write to asm file
                 asm_file.write(asm_doc)
-                for line in asm_block:
-                    asm_file.write(f"{line}\n")
                 if asm_block:
+                    asm_file.writelines(f"{line}\n" for line in asm_block)
                     asm_file.write('\n')  # separate the blocks
 
 
 def main():
     '''The program's main interface'''
-    input = sys.argv[1]
+    input = Path(sys.argv[1])
 
-    if input[-3:] == ".vm":
-        asm_filepath = input[:-3] + ".asm"
+    if input.match("*.vm"):
+        asm_filepath = input.with_suffix('.asm')
         translate(asm_filepath, [input])
-    elif os.path.isdir(input):
-        # translated .asm file will be inside the directory
-        asm_filepath = os.path.join(input, os.path.basename(input) + ".asm")
-        for root, _, files in os.walk(input):
-            vm_files = [os.path.join(root, vm_file)
-                        for vm_file in fnmatch.filter(files, '*.vm')]
+
+    elif input.is_dir():
+        # translated .asm file will be inside this directory
+        vm_files = list(input.glob("*.vm"))
         if vm_files:
+            asm_filepath = input / f"{input.name}.asm"
             translate(asm_filepath, vm_files)
         else:
             raise ValueError("Directory does not contain any .vm file")
     else:
         raise ValueError("Not a .vm file nor a directory")
-
-    # print the translated .asm file
-    with open(asm_filepath) as asm_file:
-        print(asm_file.read(), end="")
 
 
 if __name__ == "__main__":
--- compiler.py
+++ compiler.py
@@ -1,67 +1,70 @@
-import mem_access
-import calculation as calc
-import function
+from typing import Tuple, List
+
+import calculate
 import branching
+import function
+import mem_access
 
-pointers_base = [(256, 'SP'), (300, 'LCL'), (400, 'ARG'), (3000, 'THIS'), (3010, 'THAT')]
 
-# Hack bootstrap code
-bootstrap = (
-    "// bootstrap",
-    '',
-    *[(f"@{address}" "\n"
-        "D=A"        "\n"
-       f"@{pointer}" "\n"
-        "M=D"        "\n") for address, pointer in pointers_base],
-    #*function.write_call('Sys.init', 0, 0),
-)  # TODO: enable call to Sys.init after passing SimpleFunction test & before NestedCall test
+pointers_base = ((256, 'SP'), (300, 'LCL'), (400, 'ARG'), (3000, 'THIS'), (3010, 'THAT'))
+call_sys_init, _ = function.write_call('Sys.init', '0', {'function': 'Boot.0', 'call_count': 0})
+bootstrap_dir = tuple(
+    (f"@{address}" "\n"
+      "D=A"        "\n"
+     f"@{pointer}" "\n"
+      "M=D"        "\n") for address, pointer in pointers_base
+) + call_sys_init
 
-# Dispatch table for commands
-compile = {
-    'add': calc.write_add,
-    'sub': calc.write_sub,
-    'and': calc.write_and,
-    'or' : calc.write_or,
-    'eq' : calc.write_eq,
-    'gt' : calc.write_gt,
-    'lt' : calc.write_lt,
-    'not': calc.write_not,
-    'neg': calc.write_neg,
+# Commands that do not depend on or modify the state
+stateless = {
+    '//': lambda: (),  # comments do nothing...
 
-    'push': mem_access.write_push,
-    'pop' : mem_access.write_pop,
+    'add': calculate.write_add,
+    'sub': calculate.write_sub,
+    'and': calculate.write_and,
+    'or' : calculate.write_or,
+    'not': calculate.write_not,
+    'neg': calculate.write_neg,
+}
 
-    'if-goto': branching.write_if,
+# Commands that depend on and/or modify the state
+stateful = {
+    'eq' : calculate.write_eq,
+    'gt' : calculate.write_gt,
+    'lt' : calculate.write_lt,
+
     'label'  : branching.write_label,
     'goto'   : branching.write_goto,
+    'if-goto': branching.write_if,
 
     'function': function.write_function,
     'call'    : function.write_call,
     'return'  : function.write_return,
+
+    'push': mem_access.write_push,
+    'pop' : mem_access.write_pop,
 }
 
-# Groups of commands that are scoped
-branching_commands = {'label', 'goto', 'if-goto'}
-mem_access_commands = {'push', 'pop'}
+
+def parse(vm_line: str) -> Tuple[str, List[str]]:
+    '''Tokenize and parse the source vm line'''
+    tokens = vm_line.split("//")[0].split()  # ignore comments
+    try:
+        command, *args = tokens
+    except ValueError:              # blank line
+        command, args = '//', []    # no difference from a comment
+    
+    return (command, args)
 
 
-def compile_line(command: str, args: list, scope: dict, count: dict) -> tuple:
-    if command == '//':
-        return ()
+def compile_line(vm_line: str, state: dict) -> Tuple[Tuple[str, ...], dict]:
+    # Parse
+    command, args = parse(vm_line)
 
-    elif command in count:
-        return compile[command](*args, count[command])
-
-    elif command in mem_access_commands and args[0] == 'static':
-        # accessing static segment is scoped by filename
-        static_label = f"{scope['filename']}.{args[1]}"
-        return compile[command]('static', static_label)
-
-    elif command in branching_commands:
-        # branching is scoped by label's function
-        current_function = scope['function'][-1]
-        branching_label = f"{current_function}.{args[0]}"
-        return compile[command](branching_label)
-
+    # Compile!
+    if command in stateless:
+        asm_block = stateless[command]()
+        return asm_block, state
     else:
-        return compile[command](*args)
+        asm_block, new_state = stateful[command](*args, state)
+        return asm_block, new_state
